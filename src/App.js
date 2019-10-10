508a4efcf8957cfb8316c8926b34722aed8be929
Matthew Cross <mattyc246@gmail.com>
2019-10-10 11:54:43 +0800

    clicker app complted

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import FancyDisplay from "./components/FancyDisplay";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      clicks: 0
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      clicks: Math.floor(Math.random() * 10)
+    });
+  }
+
+  handleClick = () => {
+    this.setState({
+      clicks: this.state.clicks + 1
+    });
+  };
+
+  handleDecrease = () => {
+    this.setState({
+      clicks: this.state.clicks - 1
+    });
+  };
+
+  render() {
+    const { clicks } = this.state;
+    return (
+      <>
+        <div className="container-fluid h-100">
+          <div className="center-box h-75 w-75 border border-light rounded">
+            <div className="centered">
+              <h1 className="text-light text-center">Simple Clicker App</h1>
+              <h3 className="text-light text-center m-3">
+                {clicks > 0
+                  ? `You have clicked ${clicks} time${clicks === 1 ? "" : "s"}`
+                  : "You have not clicked yet!"}
+              </h3>
+              <div className="d-flex flex-row justify-content-center mx-auto w-50">
+                {clicks > 0 ? (
+                  <button
+                    className="btn btn-outline-warning mr-3"
+                    onClick={this.handleDecrease}
+                  >
+                    Decrease
+                  </button>
+                ) : (
+                  ""
+                )}
+                <button
+                  className={`btn btn-outline-${
+                    clicks > 5 ? "danger" : "success"
+                  }`}
+                  onClick={this.handleClick}
+                >
+                  {clicks > 0 ? "Increase!" : "Click Me!"}
+                </button>
+              </div>
+              <FancyDisplay
+                text={
+                  clicks > 0
+                    ? "Thanks for clicking me!"
+                    : "Go ahead and give me a click!"
+                }
+              />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FancyDisplay.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FancyDisplay = ({ text }) => {
+  return (
+    <>
+      <h5 className="text-light text-center m-3">{text}</h5>
+    </>
+  );
+};
+
+export default FancyDisplay;
